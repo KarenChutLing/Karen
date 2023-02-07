096a83e42a08c70e7bd731dc367037eb40765bc5
Karen Chut Ling <karenchutling03@gmail.com>
2023-02-07 16:16:17 +0800

    semakan pertama

---
+++ Hello.py
@@ -0,0 +1,16 @@
+#Karen Chut Ling
+#0167085945
+#karenchutling03@gmail.com
+
+name = input (" What is user name ? \n ")
+greeting = " hello " + name
+print (greeting)
+price_good= float (input ("How much the price amount of good ? \n " ))
+sales_tax = float (input ("How much the percent of sale ? \n "))
+#sale amount = total sales + sales tax
+sales_tax = (price_good * (sales_tax/100))
+total_amount_to_pay = price_good + sales_tax
+print ("Total = "+ str (total_amount_to_pay"))
+
+
+
---
+++ celsius.py
@@ -0,0 +1,8 @@
+#Karen Chut Ling
+#20DDT21F1009
+#celsius to Fahrenheit = F = C * 9/5 + 32
+C = int (input ("What is the temperature ? \n"))
+tempFahrenheit  = C * (9/5) + 32
+print(tempFahrenheit)
+
+
